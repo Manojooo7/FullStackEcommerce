e124425d6fa60d84181b2e1bf85c6a46a6d84fae
Manoj Mondal <manojmondal9834@gmail.com>
2025-08-31 22:52:08 +0530

    drizzle setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,10 @@
-import express from 'express'
+import express, {json, urlencoded} from 'express'
 import productsRouter from './routes/products/index'
 
 const app = express();
+app.use(urlencoded({extended: false}))
+app.use(json())
+
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
