e58446854160808f21a88900e382212779db6f4c
Manoj Mondal <manojmondal9834@gmail.com>
2025-09-01 23:25:43 +0530

    Implemented crud operation

--- api/src/routes/products/productController.ts
+++ api/src/routes/products/productController.ts
@@ -1,13 +1,36 @@
+import { eq } from "drizzle-orm";
 import { Request, Response } from "express"
 import { db } from 'src/db'
 import { productTable } from 'src/db/productsSchema'
 
-export function listProducts(req:Request, res:Response){
-    res.send("This is the list is product")
+export async function listProducts(req:Request, res:Response){
+    try {
+        const product = await db.select().from(productTable);
+        res.json(product)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 
-export function getProductById(req:Request, res:Response){
-    res.send(`Details of product: ${req.params.id}`)
+export async function getProductById(req:Request, res:Response){
+    const id = req.params.id
+    if(!id){
+        res.status(400).json({message: "invalid request"})
+    }
+    try {
+        const [product] = await db
+        .select()
+        .from(productTable)
+        .where(eq(productTable.id, id))
+
+        if(!product){
+            res.status(404).json({messaage: "Product not found"})
+        }else{
+            res.json(product)
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 
 export async function createProduct(req:Request, res:Response){
@@ -23,7 +46,10 @@ export async function createProduct(req:Request, res:Response){
             category: Array.isArray(req.body.category) ? req.body.category : [req.body.category],
             tags: Array.isArray(req.body.tags) ? req.body.tags : [req.body.tags]
         };
-        const product = await db.insert(productTable).values(productData).returning()
+        const product = await db
+        .insert(productTable)
+        .values(productData)
+        .returning()
 
         res.status(201).json({
             message: "Product created successfully",
@@ -37,10 +63,54 @@ export async function createProduct(req:Request, res:Response){
     }
 }
 
-export function updateProduct(req:Request, res:Response){
-    res.send(`Details of product: ${req.params.id} is updated`)
+export async function updateProduct(req:Request, res:Response){
+    
+    const id = req.params.id
+    const updatedData = req.body
+    
+    if(!id || !updatedData){
+        res.status(400).json({message: "invalid request"})
+    }
+
+    try {
+        const [product] = await db
+        .update(productTable)
+        .set(updatedData)
+        .where(eq(productTable.id, id))
+        .returning()
+
+        if(product){
+            res.status(200).json(product)
+        }else{
+            res.status(404).json({message: "Product not found"})
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 
-export function deleteProduct(req:Request, res:Response){
-    res.send(`Product ${req.params.id} has been deleted`)
+export async function deleteProduct(req:Request, res:Response){
+    
+    const id = req.params.id
+    
+    if(!id){
+        res.status(400).json({message: "invalid request"})
+    }
+
+    try {
+       const [deletedProduct] = await db
+       .delete(productTable)
+       .where(eq(productTable.id, id))
+       .returning();
+
+       if(deletedProduct){
+           res.status(204).json({message: "Product has been moved/deleted"})
+       }else{
+        res.status(404).json({message: "Product not found"})
+       }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
